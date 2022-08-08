80de2032e1ad01c331afb55523db1fcf02d0a270
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2022-08-08 15:16:05 -0400

    API FLASK

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+app.config['DEBUB'] = True
+app.config['ENV'] = 'development'
+
+CORS(app)
+
+@app.route('/')
+def main():
+    return jsonify({ "msg": "API Rest Flask"}), 200
+
+if __name__ == '__main__':
+    app.run()
